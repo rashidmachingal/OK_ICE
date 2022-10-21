cf99c1491d2b8440dbd70aa5fd08e58951b17b00
Rashid Machingal <rashileocontact@gmail.com>
2022-10-21 23:17:01 +0530

    faq section added

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,20 @@
+//navbar
 const toggleButton = document.getElementsByClassName("toggle")[0]
 const mobileItems = document.getElementsByClassName('mobile-items')[0]
 
 toggleButton.addEventListener("click",()=>{
     mobileItems.classList.toggle("active")
+})
+
+// faq
+const faqs = document.querySelectorAll(".faq")
+
+faqs.forEach(faq => {
+    faq.addEventListener("click", () => {
+        if(!faq.classList.contains("open")){
+            faqs.forEach(faq => {
+                faq.classList.remove('open');
+            })}
+        faq.classList.toggle("open")
+    })
 })
