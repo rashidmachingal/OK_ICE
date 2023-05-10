563bd95622124bd623291f9f1ac12536647a05e0
Rashid Machingal <90792555+rashidmachingal@users.noreply.github.com>
2023-05-11 04:19:26 +0530

    Update count.js

--- scripts/count.js
+++ scripts/count.js
@@ -1,8 +1,31 @@
-
-
-<script>
-function myFunction() {
-    // Call the function from your external JavaScript file
-    console.log('Hello, world!');
+/*<![CDATA[*/
+  
+const startButton = document.getElementById('start-button'); const countdownContainer = document.getElementById('countdown-container');
+const countdownTimer = document.getElementById('countdown-timer'); const secretCode = document.getElementById('secret-code');
+let timerValue = 30; let timerInterval; secretCode.style.display = 'none';
+function updateTimer() {
+  timerValue--;
+  countdownTimer.textContent = timerValue;
+  if (timerValue === 0) {
+    clearInterval(timerInterval);
+    secretCode.style.display = 'block';
+    countdownContainer.removeChild(countdownTimer);
+  }
 }
-</script>
+function startCountdown() {
+  startButton.disabled = true;
+  countdownTimer.style.display = 'block';
+  countdownTimer.textContent = timerValue;
+  timerInterval = setInterval(updateTimer, 1000);
+  document.addEventListener("visibilitychange", function() {
+    if (document.hidden) {
+      clearInterval(timerInterval);
+    } else {
+      timerInterval = setInterval(updateTimer, 1000);
+    }
+  });
+}
+  
+startButton.addEventListener('click', startCountdown);
+
+ /*]]>*/
